bbca70ae7b61090f7f1eade6f29ea1315d4319b6
Jacob Moore <jacobashymo@MacBook-Air-827.local>
2018-10-24 18:59:43 -0500

    Hopefully ready to go

--- bamazon-manager.js
+++ bamazon-manager.js
@@ -5,13 +5,13 @@ var mysql = require('mysql');
 // Define the MySQL connection parameters
 var connection = mysql.createConnection({
 	host: 'localhost',
-	port: 3306,
+	port: 8889,
 
 	// Your username
 	user: 'root',
 
 	// Your password
-	password: '',
+	password: 'root',
 	database: 'Bamazon'
 });
 
